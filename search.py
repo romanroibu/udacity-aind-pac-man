ced510d7c761c78990871c44f452c3d6485990af
Roman Roibu <romanroibu@gmail.com>
2017-04-24 10:05:38 +0200

    Implement Uniform cost search

--- search.py
+++ search.py
@@ -182,8 +182,52 @@ def breadthFirstSearch(problem):
 
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+
+  start = problem.getStartState()
+  startPath = initPath(start)
+
+  # step 1: initialize the frontier using the initial state of the problem
+  frontier = util.PriorityQueue()
+  frontierSet = set()
+
+  frontier.push(startPath, 0)
+  frontierSet.add(start)
+
+  # step 2: initialize the explored set to be empty
+  explored = set()
+
+  while True:
+    # step 3: if the frontier is empty, return failure
+    if frontier.isEmpty():
+      return False
+
+    # step 4: choose a leaf node and remove it from the frontier
+    path = frontier.pop()
+    target  = pathTarget(path)
+    actions = pathActions(path)
+    frontierSet.remove(target)
+
+    # step 5: if the node contans a goal state, return the corresponding solution
+    if problem.isGoalState(target):
+      return actions
+
+    # step 6: add the node to the explored set
+    explored.add(target)
+
+    # step 7: expand the node
+    successors = problem.getSuccessors(target)
+
+    # step 8: add the resulting nodes to the frontier, if not in frontier or explored set
+    for nextPath in map(makePath, successors):
+      nextTarget = pathTarget(nextPath)
+      if not nextTarget in explored and not nextTarget in frontierSet:
+        nextActions = actions + pathActions(nextPath)
+        nextPriority = problem.getCostOfActions(nextActions)
+        nextCost = pathCost(nextPath)
+        nextTuple = (nextTarget, nextActions, nextCost)
+
+        frontier.push(nextTuple, nextPriority)
+        frontierSet.add(nextTarget)
 
 def nullHeuristic(state, problem=None):
   """
